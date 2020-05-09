1e36268f5a92beb587bf55daa4aabfee9df6bbc5
codeyhj <colgateyhj@gmail.com>
2020-05-09 08:21:04 +0800

    ✨ feat(merge): merge
    merge

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
-var engine = require("./engine");
-var conventionalCommitTypes = require("./emoji.js");
+const engine = require("./engine");
+const fs = require("fs");
+let conventionalCommitTypes = require("./emoji.js").emojis;
+const packagePath = process.cwd() + "package.json";
+if (fs.existsSync(packagePath)) {
+  const packjson = require(packagePath);
+  const config = packjson.config;
+  if (config && config.hasOwnProperty("emojiPath")) {
+    conventionalCommitTypes = require(packjson.config.emojiPath);
+  }
+}
 
 module.exports = engine({
-  types: conventionalCommitTypes.emojis
+  types: conventionalCommitTypes,
 });
